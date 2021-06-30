86b246d1faa0555a4881416e6da4ed9ecb4f5602
Romual Naibaho <romualnaibaho26@gmail.com>
2021-06-30 22:40:07 +0700

    js

---
+++ tugas6.js
@@ -0,0 +1,12 @@
+function perulangan(){
+
+    for(var i = 1; i <= 20; i++){
+        if(i % 2 == 0){
+            console.log("Bilangan Genap");
+        }else{
+            console.log(i);
+        }
+    }
+}
+
+perulangan();
